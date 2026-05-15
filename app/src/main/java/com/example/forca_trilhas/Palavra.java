31edf892cae9351372a6f69f7290c380441dff64
mateussantos <mateus.santos@ifsuldeminas.edu.br>
2026-05-15 15:24:32 -0300

    Versão 15/05/2026

---
+++ app/src/main/java/com/example/forca_trilhas/Palavra.java
@@ -0,0 +1,21 @@
+package com.example.forca_trilhas;
+
+public class Palavra {
+    private String nome, categoria;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
--- app/src/main/java/com/example/forca_trilhas/TelaCadastro.java
+++ app/src/main/java/com/example/forca_trilhas/TelaCadastro.java
@@ -1,15 +1,29 @@
 package com.example.forca_trilhas;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaCadastro extends AppCompatActivity {
+public class TelaCadastro extends AppCompatActivity implements View.OnClickListener {
+    private Button btnCadastro, btnListar;
+    private EditText caixaPalavra;
+    private RadioGroup grupo;
+    private String categoriaSelecionada;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +34,45 @@ public class TelaCadastro extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnCadastro = findViewById(R.id.button2);
+        btnCadastro.setOnClickListener(this);
+        btnListar = findViewById(R.id.button3);
+        btnListar.setOnClickListener(this);
+        caixaPalavra = findViewById(R.id.editTextText);
+        categoriaSelecionada = "Objeto Escolar";
+        grupo = findViewById(R.id.id_grupo);
+        grupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup radioGroup, int i) {
+                if(radioGroup == grupo){
+                    if(i == R.id.radioButton)
+                    {
+                        categoriaSelecionada = "Frutas";
+                    }
+                    if(i == R.id.radioButton2)
+                    {
+                        categoriaSelecionada = "Ferramentas";
+                    }
+                    if(i == R.id.radioButton3)
+                    {
+                        categoriaSelecionada = "Objeto Escolar";
+                    }
+                    if(i == R.id.radioButton4)
+                    {
+                        categoriaSelecionada = "Eletrodoméstico";
+                    }
+                    if(i == R.id.radioButton5)
+                    {
+                        categoriaSelecionada = "Esportes";
+                    }
+                }
+                Toast.makeText(TelaCadastro.this, categoriaSelecionada, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
